9a3c65e25f0dfd2cffd17eb6356801c4dc73a99d
Pawan Sharma <pawan.sharma@valuecoders.com>
2022-12-20 15:27:33 +0530

    54 Adding data from Azure Functions to queue

---
+++ AZFunctions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AZFunctions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- AZFunctions/WriteSalesRequestToQueue.cs
+++ AZFunctions/WriteSalesRequestToQueue.cs
@@ -6,7 +6,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Newtonsoft.Json; 
+using AZFunctions.Models;
+using Microsoft.Azure.WebJobs.Description;
 
 namespace AZFunctions
 {
@@ -15,19 +17,16 @@ namespace AZFunctions
         [FunctionName("WriteSalesRequestToQueue")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Queue("salesrequestinboundtest",Connection = "AzureWebJobsStorage")] IAsyncCollector<SalesRequest>salesRequestQueue,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("C# HTTP trigger function processed a request.");         
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            SalesRequest data = JsonConvert.DeserializeObject<SalesRequest>(requestBody);
+            //name = name ?? data?.name;
+            await salesRequestQueue.AddAsync(data);
+            string responseMessage ="SalesRequest has been received for data "+data.Name;
 
             return new OkObjectResult(responseMessage);
         }
